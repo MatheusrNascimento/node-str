08278c3d4bac89877bcc9449bb49c516c7e27d96
Matheus R Nascimento <matheusrnascimento92@gmail.com>
2024-07-14 17:45:28 -0300

    feat: Adicionando endpoint de Api Rest na api

--- src/app.js
+++ src/app.js
@@ -2,18 +2,55 @@
 
 //Importa o express para o arquivo
 const express = require('express');
+// Cria uma variavel e insere os recursos para configuração de rotas da API
+const bodyparser = require('body-parser');
 // Cria uma aplicação express
 const app = express();
-// Cria uma variavel e insere os recursos para configuração de rotas da API
+// Cria uma variavel e importa o pacote body-parser
 const router = express.Router();
 
-// Cria um endpoint do tipo get com o caminho de /
+// Adiciona um middleware que usa o body-parser para tratar todas as requisiçoes que chegarem na API
+app.use(bodyparser.json());
+// Adiciona um middleware que adiciona codificação de URL ao body-parser
+app.use(bodyparser.urlencoded({ extended: false}));
+
+// Cria um endpoint do tipo get
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
-        title: "Minha primeira API em node KeK",
+        title: "Api rodando...",
         version: "0.0.1"
     });
 });
+
+// Cria um endpoint do tipo post
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+// Cria um endpoint do tipo put que espera receber um Id passado por a URL
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    req.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+// Cria um endpoint do tipo delete
+const del = router.delete('/:id', (req, res, next) => {
+    res.status(201).send({ 
+        message: 'Foi deletado'
+    });
+});
+
+// Adiciona um Middleware que expõem o endpoint ~route~ com o caminho indicando o caminho '/' para acessalo
 app.use('/', route);
+// Adiciona um Middleware que expõem o endpoint ~create~ com o caminho indicando o caminho '/products' para acessalo
+app.use('/products', create)
+// Adiciona um Middleware que expõem o endpoint ~put~ com o caminho indicando o caminho '/products/:id' para acessalo
+app.use('/products', put);
+// Adiciona um Middleware que expõem o endpoint ~del~ com o caminho indicando o caminho '/products/:id' para acessalo
+app.use('/products', del);
 
 module.exports = app;
